65dd1aefbb2c52ebd7e2838669012733f184bef3
Jobie Wong <jobietsy@gmail.com>
2022-05-08 12:40:20 +0100

    fixed item width

--- src/index.js
+++ src/index.js
@@ -10,15 +10,15 @@ if (require('electron-squirrel-startup')) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 900,
+    height: 800,
   });
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
