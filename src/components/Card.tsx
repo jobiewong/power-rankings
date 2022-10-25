de8f1082d514d2c208d4fea4a00c72889436be00
Jobie Wong <jobietsy@gmail.com>
2022-10-25 19:49:59 +0100

    motion

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,27 +1,63 @@
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
+import { motion } from "framer-motion";
 import React from "react";
 
 type CardProps = { team: string };
 
 const Card = (props: any) => {
-  const { attributes, listeners, setNodeRef, transform, transition } =
+  const { attributes, listeners, setNodeRef, transform, isDragging } =
     useSortable({ id: props.id });
 
   const style = {
     transform: CSS.Transform.toString(transform),
-    transition,
+    transition: null,
   };
 
-  console.log(props.team.bgCol);
+  const baseStyles: React.CSSProperties = {
+    position: "relative",
+    width: 140,
+    height: 140,
+  };
+
+  const initialStyles = {
+    x: 0,
+    y: 0,
+    scale: 1,
+  };
 
   return (
-    <div
+    <motion.div
       ref={setNodeRef}
       style={style}
       {...attributes}
       {...listeners}
       className="mb-4 flex h-16 w-full flex-row pl-14"
+      animate={
+        transform
+          ? {
+              x: transform.x,
+              y: transform.y,
+              scale: isDragging ? 1.05 : 1,
+              zIndex: isDragging ? 1 : 0,
+              boxShadow: isDragging
+                ? "0 0 0 1px rgba(63, 63, 68, 0.05), 0px 15px 15px 0 rgba(34, 33, 81, 0.25)"
+                : undefined,
+            }
+          : initialStyles
+      }
+      transition={{
+        duration: !isDragging ? 0.25 : 0,
+        easings: {
+          type: "spring",
+        },
+        scale: {
+          duration: 0.25,
+        },
+        zIndex: {
+          delay: isDragging ? 0 : 0.25,
+        },
+      }}
     >
       <div
         className={`flex w-full flex-row rounded-sm`}
@@ -37,7 +73,7 @@ const Card = (props: any) => {
           {props.team.name}
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
