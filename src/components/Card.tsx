09243d64163e83be20232e84c58a98e2c151d5cc
jobiewong <jobietsy@hotmail.co.uk>
2022-10-27 23:09:11 +0100

    framer motion tweaks

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,22 +9,6 @@ const Card = (props: any) => {
   const { attributes, listeners, setNodeRef, transform, isDragging } =
     useSortable({ id: props.id, transition: null });
 
-  const style = {
-    transform: CSS.Transform.toString(transform),
-  };
-
-  // const baseStyles: React.CSSProperties = {
-  //   position: "relative",
-  // };
-
-  // const initialStyles = {
-  //   set: {
-  //     x: 0,
-  //     y: 0,
-  //     scale: 1,
-  //   },
-  // };
-
   const baseStyles: React.CSSProperties = {
     position: "relative",
   };
@@ -35,48 +19,24 @@ const Card = (props: any) => {
     scale: 1,
   };
 
-  const stylesItem = "mb-4 flex h-16 w-full flex-row pl-14";
+  const stylesItem = "mb-4 flex h-16 w-full flex-row";
 
   return (
-    <motion.div
-      className={stylesItem}
-      style={baseStyles}
-      ref={setNodeRef}
-      layoutId={props.id}
-      animate={
-        transform
-          ? {
-              x: transform.x,
-              y: transform.y,
-              scale: isDragging ? 1.05 : 1,
-              zIndex: isDragging ? 1 : 0,
-            }
-          : initialStyles
-      }
-      transition={{
-        duration: !isDragging ? 0.25 : 0,
-        easings: {
-          type: "spring",
-        },
-        scale: {
-          duration: 0.25,
-        },
-        zIndex: {
-          delay: isDragging ? 0 : 0.25,
-        },
-      }}
-      {...attributes}
-      {...listeners}
-    >
+    <div className="flex flex-row">
+      <div className="pointer-events-none h-0 w-16 bg-transparent"></div>
+
       <motion.div
-        className={`flex w-full flex-row rounded-sm`}
-        style={{
-          color: props.team.textCol,
-          backgroundColor: props.team.bgCol,
-        }}
+        className={stylesItem}
+        style={baseStyles}
+        ref={setNodeRef}
+        layoutId={props.id}
         animate={
           transform
             ? {
+                x: transform.x,
+                y: transform.y,
+                scale: isDragging ? 1.05 : 1,
+                zIndex: isDragging ? 1 : 0,
                 boxShadow: isDragging
                   ? "0 0 0 1px rgba(63, 63, 68, 0.05), 0px 15px 15px 0 rgba(0, 0, 0, 0.25)"
                   : undefined,
@@ -85,21 +45,34 @@ const Card = (props: any) => {
         }
         transition={{
           duration: !isDragging ? 0.25 : 0,
-          easings: {
-            type: "spring",
+          damping: 15,
+          type: "spring",
+          scale: {
+            duration: 0.25,
+          },
+          zIndex: {
+            delay: isDragging ? 0 : 0.25,
           },
         }}
         {...attributes}
         {...listeners}
       >
-        <div className="flex h-full w-16 flex-col items-center justify-center rounded-l-sm bg-zinc-400">
-          logo
-        </div>
-        <div className="flex items-center px-4 text-2xl font-black uppercase">
-          {props.team.name}
+        <div
+          className={`flex w-full flex-row rounded-sm`}
+          style={{
+            color: props.team.textCol,
+            backgroundColor: props.team.bgCol,
+          }}
+        >
+          <div className="flex h-full w-16 flex-col items-center justify-center rounded-l-sm bg-zinc-400">
+            logo
+          </div>
+          <div className="flex items-center px-4 text-2xl font-black uppercase">
+            {props.team.name}
+          </div>
         </div>
       </motion.div>
-    </motion.div>
+    </div>
   );
 };
 
--- src/components/Rankings.tsx
+++ src/components/Rankings.tsx
@@ -20,8 +20,8 @@ const Rankings = () => {
       <div className="w-[66vw]">
         <div className="columns-2 gap-0">
           <div className="pointer-events-none absolute w-[66vw] columns-2 gap-0 ">
-            {numbers.map((number) => (
-              <Numbers key={number} number={number} />
+            {numbers.map((number, index) => (
+              <Numbers key={number} number={number} index={index} />
             ))}
           </div>
           {/* <Form /> */}
@@ -36,13 +36,32 @@ export default Rankings;
 
 type numbersProps = {
   number: number;
+  index: number;
 };
 
 // define numbers component
-const Numbers = ({ number }: numbersProps) => {
+const Numbers = ({ number, index }: numbersProps) => {
   return (
-    <div className="relative mb-4 flex h-16 w-14 items-center justify-center text-2xl font-black">
+    <div className="relative mb-4 flex h-16 w-14 flex-col items-center justify-center text-2xl font-black text-white">
       {number}
+      {index === 0 && (
+        <div className="absolute flex translate-y-6 flex-col items-center">
+          <div className="mb-1 h-[.2rem] w-8 bg-[#CBAE39]" />
+          <div className="h-[.2rem] w-5 bg-[#CBAE39]" />
+        </div>
+      )}
+      {index === 1 && (
+        <div className="absolute flex translate-y-6 flex-col items-center">
+          <div className="mb-1 h-[.2rem] w-8 bg-[#D8D8D8]" />
+          <div className="h-[.2rem] w-5 bg-[#D8D8D8]" />
+        </div>
+      )}
+      {index === 2 && (
+        <div className="absolute flex translate-y-6 flex-col items-center">
+          <div className="mb-1 h-[.2rem] w-8 bg-[#AC8A61]" />
+          <div className="h-[.2rem] w-5 bg-[#AC8A61]" />
+        </div>
+      )}
     </div>
   );
 };
