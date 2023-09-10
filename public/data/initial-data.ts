36edce8e2062d36e2554e455e443096e58affab7
Jobie Wong <jobietsy@gmail.com>
2023-09-10 16:08:49 -0400

    📦 NEW: Re-add team images

---
+++ public/data/initial-data.ts
@@ -0,0 +1,6 @@
+const initialTeams = {
+    teams: ['faze', 'sen', 'cloudnine', 'nrg', 'eg', 'loud', 'mibr', 'thieves', 'furia', 'leviatan'],
+    overflow: ['hgvs']
+};
+
+export default initialTeams;
---
+++ public/data/team-type.ts
@@ -0,0 +1,9 @@
+type teamProps = {
+    id: string;
+    name: string;
+    bgCol: string;
+    textCol: string;
+    logo: string;
+  };
+
+export type { teamProps };
