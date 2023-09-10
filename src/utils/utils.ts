ef77f791acc2b80a449f245429f20bf1369caa2d
Jobie Wong <jobietsy@gmail.com>
2023-09-10 16:53:30 -0400

    📦 NEW: Ranking Items and grid components

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,20 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { faker } from "@faker-js/faker";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function generateData(number: number) {
+  const data = [];
+  for (let i = 0; i < number; i++) {
+    data.push({
+      id: faker.string.uuid(),
+      name: faker.person.fullName(),
+      textColour: faker.internet.color(),
+      backgroundColour: faker.internet.color(),
+    });
+  }
+  return data;
 }
